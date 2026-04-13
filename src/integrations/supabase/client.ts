23c94326ef0532c04242aaa4524aa3708da86927
MaiconPithon <160951714+MaiconPithon@users.noreply.github.com>
2026-04-13 20:32:39 -0300

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,9 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
-
+const SUPABASE_URL = "https://gdmfeeggevwrujoqzzgx.supabase.co";
+const SUPABASE_KEY = "sb_publishable_jkGVF7f-XVlQq_LbaqXDDA_WZawfiQn";
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
 
